fc55bb8a245fb8fc824c9d3aece0150caa94a9ab
srom <romain.strock@gmail.com>
2016-07-31 21:14:44 +0100

    Adjust physics parameters

--- src/constants.js
+++ src/constants.js
@@ -9,8 +9,8 @@ export const STAGE_WIDTH_M = 5;
 export const RATIO = STAGE_WIDTH_M / STAGE_WIDTH_PX;
 
 export const FRAME_RATE = 1 / 60;
-export const VELOCITY_ITERATIONS = 8;
-export const POSITION_ITERATIONS = 3;
+export const VELOCITY_ITERATIONS = 6;
+export const POSITION_ITERATIONS = 2;
 
 export const DEBUG_PHYSICS = false;
 export const DEBUG_CANVAS_CLASS = "debugCanvas";
