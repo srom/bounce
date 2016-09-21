d525435c33acedbb85b3e1cf38a158b05028ebf5
srom <romain.strock@gmail.com>
2016-09-22 00:26:09 +0100

    Prevent ball from starting with a wrong angle

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const WALL_THICKNESS = 8;
 export const STAGE_WIDTH_M = 5;
 export const RATIO = STAGE_WIDTH_M / STAGE_WIDTH_PX;
 
-export const VELOCITY_FACTOR = 2;
+export const VELOCITY_FACTOR = 2.3;
 
 export const FRAME_RATE = 1 / 60;
 export const VELOCITY_ITERATIONS = 6;
