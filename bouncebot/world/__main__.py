44a3a11127cc9983dd9fc59e16e3ecbbdd5d1b3d
srom <romain.strock@gmail.com>
2018-02-06 23:08:45 +0000

    Debug main

--- bouncebot/world/__main__.py
+++ bouncebot/world/__main__.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from google.protobuf.json_format import MessageToJson
 
 from query import simulation
+from reward import get_reward
 from models.world_pb2 import Worlds, HOLD
 
 NUM_FRAMES = 600
@@ -11,21 +12,25 @@ NUM_FRAMES = 600
 def main():
     result = None
     action = HOLD
-    movie = True
+    # movie = True
 
-    all_worlds = Worlds()
+    action = simulation(result, action)
 
-    frame = 0
-    while frame < NUM_FRAMES:
-        res_worlds = simulation(result, action, movie=movie)
-        frame += len(res_worlds.worlds)
-        worlds = res_worlds.worlds
-        new_result = worlds[-1]
-        if new_result.frame_nb > 0:
-            result = new_result
-            all_worlds.worlds.extend(worlds)
+    recent_rewards = []
+    print get_reward(None, action, recent_rewards)
 
-    print MessageToJson(all_worlds)
+    # all_worlds = Worlds()
+    # frame = 0
+    # while frame < NUM_FRAMES:
+    #     res_worlds = simulation(result, action, movie=movie)
+    #     frame += len(res_worlds.worlds)
+    #     worlds = res_worlds.worlds
+    #     new_result = worlds[-1]
+    #     if new_result.frame_nb > 0:
+    #         result = new_result
+    #         all_worlds.worlds.extend(worlds)
+    #
+    # print MessageToJson(all_worlds)
 
 
 if __name__ == '__main__':
