db9362f3a37f4926c035d8ecbc298ccb37e54057
srom <romain.strock@gmail.com>
2018-02-17 15:01:41 +0000

    Prepare deployment

--- deploy_bouncebot.sh
+++ deploy_bouncebot.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-pushd bouncebot
-
 TARFILE=bouncebot.tar.gz
 tar --exclude '.git*' --exclude '*.sqlite3' --exclude 'env' --exclude 'packages/*' --exclude '.idea/*' \
     --exclude '*.pyc' --exclude 'checkpoints/*' --exclude 'summary_log/*' --exclude 'model/*' \
-    --exclude 'node_modules/*' -pczvf $TARFILE .
+    --exclude 'node_modules/*' --exclude 'resources/*' --exclude 'src/*' --exclude 'vendor/*' -pczvf $TARFILE .
 
 aws s3 cp $TARFILE s3://romainstrock-packages/
-
-popd
