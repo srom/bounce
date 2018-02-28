f5b19b3e9f1500ff0e3d7d75709b000da19f9ab7
srom <romain.strock@gmail.com>
2018-02-28 23:33:35 +0000

    Log arrow state

--- bouncebot/play/__main__.py
+++ bouncebot/play/__main__.py
@@ -20,11 +20,12 @@ def main():
         worlds, won = play(bouncebot)
 
         logger.info(
-            'Game end: %s (%d worlds; %d frames; %d bricks left)',
+            'Game end: %s (%d worlds; %d frames; %d bricks left) ; Arrow ready: %s',
             'WON' if won else 'LOST',
             len(worlds.worlds),
             worlds.worlds[-1].frame_nb,
             len(worlds.worlds[-1].bricks),
+            worlds.worlds[-1].arrow.ready,
         )
 
         print MessageToJson(worlds)
